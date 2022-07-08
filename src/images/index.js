57dc3ac5dd3fb0bd08521fcbe5942c0d0ccfdf6d
anclark686 <43712089+anclark686@users.noreply.github.com>
2022-07-07 23:08:59 -0400

    Added projects3

--- src/images/index.js
+++ src/images/index.js
@@ -3,8 +3,8 @@ const IMAGES = {
   pic2: require('./Calc.png'),
   pic3: require('./ToDo.png'),
   pic4: require('./Blog.png'),
-  pic5: require('./PNE.png'),
-  pic6: require('./GM.png'),
+  pic5: require('./PNE.jpg'),
+  pic6: require('./GM.jpg'),
 }
 
 export default IMAGES;
